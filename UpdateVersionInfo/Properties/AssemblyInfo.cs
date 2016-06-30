0328fdadbaa65fd4f89fc62cf6bcf15d64a8ff87
Pieter Jansen van Vuuren <pieter.jvv@gmail.com>
2016-07-01 10:08:44 +1200

    Initial check-in

---
+++ UpdateVersionInfo/Properties/AssemblyInfo.cs
@@ -0,0 +1,37 @@
+﻿/*
+ * A copy of the UpdateVersionInfo project from SolTech, Inc.
+ * See: https://github.com/soltechinc/soltechxf
+ */
+
+using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("UpdateVersionInfo")]
+[assembly: AssemblyDescription("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("913053d6-9196-43a2-a13e-62363938810d")]
+
+#if (DEBUG)
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
+
+[assembly: AssemblyCompany("SolTech, Inc.")]
+[assembly: AssemblyProduct("Update Version Information")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: NeutralResourcesLanguage("en")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
