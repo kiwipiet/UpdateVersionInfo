6b03c96ca3ef2197949678ab09142ee091d6b462
Pieter Jansen van Vuuren <pieter.jvv@gmail.com>
2016-08-05 10:53:21 +1200

    Change the way we update droid and touch versions

--- UpdateVersionInfo/UpdateVersionInfoImpl.cs
+++ UpdateVersionInfo/UpdateVersionInfoImpl.cs
@@ -10,11 +10,11 @@ namespace UpdateVersionInfo
 {
     internal class UpdateVersionInfoImpl
     {
-        static string AssemblyVersionExpression = @"^\s*\[assembly:\s*(?<attribute>(?:System\.)?(?:Reflection\.)?AssemblyVersion(?:Attribute)?\s*\(\s*""(?<version>[^""]+)""\s*\)\s*)\s*\]\s*$";
-        static string AssemblyFileVersionExpression = @"^\s*\[assembly:\s*(?<attribute>(?:System\.)?(?:Reflection\.)?AssemblyFileVersion(?:Attribute)?\s*\(\s*""(?<version>[^""]+)""\s*\)\s*)\s*\]\s*$";
+        private const string AssemblyVersionExpression = @"^\s*\[assembly:\s*(?<attribute>(?:System\.)?(?:Reflection\.)?AssemblyVersion(?:Attribute)?\s*\(\s*""(?<version>[^""]+)""\s*\)\s*)\s*\]\s*$";
+        private const string AssemblyFileVersionExpression = @"^\s*\[assembly:\s*(?<attribute>(?:System\.)?(?:Reflection\.)?AssemblyFileVersion(?:Attribute)?\s*\(\s*""(?<version>[^""]+)""\s*\)\s*)\s*\]\s*$";
 
-        static readonly Regex assemblyVersionRegEx = new Regex(AssemblyVersionExpression, RegexOptions.Multiline | RegexOptions.Compiled);
-        static readonly Regex assemblyFileVersionRegEx = new Regex(AssemblyFileVersionExpression, RegexOptions.Multiline | RegexOptions.Compiled);
+        private static readonly Regex AssemblyVersionRegEx = new Regex(AssemblyVersionExpression, RegexOptions.Multiline | RegexOptions.Compiled);
+        private static readonly Regex AssemblyFileVersionRegEx = new Regex(AssemblyFileVersionExpression, RegexOptions.Multiline | RegexOptions.Compiled);
 
         /// <summary>
         /// Shows help/usage information
@@ -31,32 +31,30 @@ namespace UpdateVersionInfo
         [Verb(IsDefault = true)]
         public static void Version(
             [DefaultValue(1), MoreOrEqualTo(0), Description("A numeric major version number greater than zero."), Aliases("v")]int major,
-            [DefaultValue(0), MoreOrEqualTo(0), Description("A numeric minor number greater than zero."), Aliases("m")]int minor,
-            [DefaultValue(0), MoreOrEqualTo(0), Description("A numeric build number greater than zero."), Aliases("b")]int build,
-            [DefaultValue(0), MoreOrEqualTo(0), Description("A numeric revision number greater than zero."), Aliases("r")]int revision,
+            [DefaultValue(0), MoreOrEqualTo(0), Description("A numeric minor number equal or greater than zero."), Aliases("m")]int minor,
+            [DefaultValue(0), MoreOrEqualTo(0), Description("A numeric build number equal or greater than zero."), Aliases("b")]int build,
+            [DefaultValue(0), MoreOrEqualTo(0), Description("A numeric revision number equal or greater than zero."), Aliases("r")]int revision,
             [Description("The path to a C# file to update with version information."), Aliases("p,path")]string versionCsPath,
             [Description("The path to an android manifest file to update with version information."), Aliases("a,androidManifest")]string androidManifestPath,
             [Description("The path to an iOS plist file to update with version information."), Aliases("t,touchPlist")]string touchPListPath)
         {
             try
             {
-                //ValidateCommandLine(commandLine);
-
-                Version version = new Version(
+                var version = new Version(
                     major,
                     minor,
                     build,
                     revision);
 
-                if (!string.IsNullOrEmpty(versionCsPath))
+                if (!string.IsNullOrEmpty(versionCsPath) && IsValidCSharpVersionFile(versionCsPath))
                 {
-                    UpdateCSVersionInfo(versionCsPath, version);
+                    UpdateCsVersionInfo(versionCsPath, version);
                 }
-                if (!string.IsNullOrEmpty(androidManifestPath))
+                if (!string.IsNullOrEmpty(androidManifestPath) && IsValidAndroidManifest(androidManifestPath))
                 {
                     UpdateAndroidVersionInfo(androidManifestPath, version);
                 }
-                if (!string.IsNullOrEmpty(touchPListPath))
+                if (!string.IsNullOrEmpty(touchPListPath) && IsValidTouchPList(touchPListPath))
                 {
                     UpdateTouchVersionInfo(touchPListPath, version);
                 }
@@ -66,19 +64,21 @@ namespace UpdateVersionInfo
                 Console.WriteLine("An unexpected error was encountered:" + e.Message);
             }
         }
-        private static void UpdateCSVersionInfo(string path, Version version)
+        private static void UpdateCsVersionInfo(string path, Version version)
         {
             string contents;
             using (var reader = new StreamReader(path))
             {
                 contents = reader.ReadToEnd();
             }
-            contents = assemblyVersionRegEx.Replace(contents, "[assembly: System.Reflection.AssemblyVersion(\"" + version.ToString() + "\")]");
-            if (assemblyFileVersionRegEx.IsMatch(contents))
+            contents = AssemblyVersionRegEx.Replace(contents,
+                $"[assembly: System.Reflection.AssemblyVersion(\"{version}\")]");
+            if (AssemblyFileVersionRegEx.IsMatch(contents))
             {
-                contents = assemblyFileVersionRegEx.Replace(contents, "[assembly: System.Reflection.AssemblyFileVersion(\"" + version.ToString() + "\")]");
+                contents = AssemblyFileVersionRegEx.Replace(contents,
+                    $"[assembly: System.Reflection.AssemblyFileVersion(\"{version}\")]");
             }
-            using (StreamWriter writer = new StreamWriter(path, false))
+            using (var writer = new StreamWriter(path, false))
             {
                 writer.Write(contents);
             }
@@ -86,24 +86,27 @@ namespace UpdateVersionInfo
 
         private static void UpdateAndroidVersionInfo(string path, Version version)
         {
-            const string androidNS = "http://schemas.android.com/apk/res/android";
-            XName versionCodeAttributeName = XName.Get("versionCode", androidNS);
-            XName versionNameAttributeName = XName.Get("versionName", androidNS);
-            XDocument doc = XDocument.Load(path);
+            const string androidNs = "http://schemas.android.com/apk/res/android";
+            var versionCodeAttributeName = XName.Get("versionCode", androidNs);
+            var versionNameAttributeName = XName.Get("versionName", androidNs);
+            var doc = XDocument.Load(path);
+            // ReSharper disable once PossibleNullReferenceException
             doc.Root.SetAttributeValue(versionCodeAttributeName, version.Build);
-            doc.Root.SetAttributeValue(versionNameAttributeName, version);
+            doc.Root.SetAttributeValue(versionNameAttributeName, $"{version.Major}.{version.Minor}");
             doc.Save(path);
         }
 
         private static void UpdateTouchVersionInfo(string path, Version version)
         {
-            XDocument doc = XDocument.Load(path);
+            var doc = XDocument.Load(path);
 
             var shortVersionElement = doc.XPathSelectElement("plist/dict/key[string()='CFBundleShortVersionString']").NextNode as XElement;
-            shortVersionElement.Value = version.Major + "." + version.Minor + "." + version.Build;
+            // ReSharper disable once PossibleNullReferenceException
+            shortVersionElement.Value = $"{version.Major}.{version.Minor}";
 
             var bundleVersionElement = doc.XPathSelectElement("plist/dict/key[string()='CFBundleVersion']").NextNode as XElement;
-            bundleVersionElement.Value = version.Build + "." + version.Revision;
+            // ReSharper disable once PossibleNullReferenceException
+            bundleVersionElement.Value = $"{version.Build}.{version.Revision}";
 
             doc.Save(path);
         }
@@ -121,7 +124,7 @@ namespace UpdateVersionInfo
                     contents = reader.ReadToEnd();
                 }
 
-                if (assemblyVersionRegEx.IsMatch(contents))
+                if (AssemblyVersionRegEx.IsMatch(contents))
                 {
                     return true;
                 }
@@ -142,8 +145,8 @@ namespace UpdateVersionInfo
             try
             {
                 // <manifest ...
-                XDocument doc = XDocument.Load(path);
-                var rootElement = doc.Root as XElement;
+                var doc = XDocument.Load(path);
+                var rootElement = doc.Root;
                 if (rootElement != null && rootElement.Name == "manifest") return true;
             }
             catch (Exception e)
@@ -161,15 +164,13 @@ namespace UpdateVersionInfo
             try
             {
                 //<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-                XDocument doc = XDocument.Load(path);
+                var doc = XDocument.Load(path);
+                // ReSharper disable once PossibleNullReferenceException
                 if (doc.DocumentType.Name == "plist")
                 {
                     var shortVersionElement = doc.XPathSelectElement("plist/dict/key[string()='CFBundleShortVersionString']");
-                    if (shortVersionElement != null)
-                    {
-                        var valueElement = shortVersionElement.NextNode as XElement;
-                        if (valueElement != null && valueElement.Name == "string") return true;
-                    }
+                    var valueElement = shortVersionElement?.NextNode as XElement;
+                    if (valueElement != null && valueElement.Name == "string") return true;
                 }
             }
             catch (Exception e)
